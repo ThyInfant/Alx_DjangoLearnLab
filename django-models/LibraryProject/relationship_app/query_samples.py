08269382991f64aae8747a4a32fb40a873e77a55
Rob Tade <robtade4@gmail.com>
2026-01-25 20:39:14 +0300

    fix typo

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 
-# -------------------------------
 # Query all books by a specific author
-# -------------------------------
-
 author_name = "George Orwell"
 author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
@@ -13,10 +10,7 @@ for book in books_by_author:
     print(book.title)
 
 
-# -------------------------------
 # List all books in a library
-# -------------------------------
-
 library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
@@ -25,10 +19,7 @@ for book in books_in_library:
     print(book.title)
 
 
-# -------------------------------
 # Retrieve the librarian for a library
-# -------------------------------
-
 library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 librarian = Librarian.objects.get(library=library)
