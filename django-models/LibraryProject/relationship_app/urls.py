6f90e5a6b83121c43f0e918a1eed4a33bb68438f
Rob Tade <robtade4@gmail.com>
2026-01-30 14:19:45 +0300

    redo somethings

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from LibraryProject.bookshelf import views
+from .views import list_books, LibraryDetailView, register_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    # Function-based view for all books
     path('books/', list_books, name='list_books'),
+
+    # Class-based view for library detail
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    # Authentication URLs
+    # function-based registration
+    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 
-from .models import Library
-from .models import Book
+from .models import Library, Book
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
