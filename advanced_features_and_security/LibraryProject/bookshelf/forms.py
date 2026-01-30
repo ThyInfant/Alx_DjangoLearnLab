dd7a2f16a82cb8a1eba3725e1016f63e4efbe761
Rob Tade <robtade4@gmail.com>
2026-01-30 21:22:55 +0300

    add features to form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+    def clean_title(self):  
+        title = self.cleaned_data.get('title')
+        if not title:
+            raise forms.ValidationError("Title cannot be empty.")
+        return title
