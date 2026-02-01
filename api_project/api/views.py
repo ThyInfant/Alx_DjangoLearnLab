74908482230a2967017b9ee884cedb1b04432efe
Rob Tade <robtade4@gmail.com>
2026-02-01 19:55:50 +0300

    Add comments

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+# from rest_framework.generics.ListAPIView
+
 
 # Create your views here.
 class BookList(ListAPIView):
